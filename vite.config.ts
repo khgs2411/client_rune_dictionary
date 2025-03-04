b8f3867f0df551d6c55692c1547b32556bcdb6bb
Liad Goren <liad@tigloo.io>
2025-03-04 22:00:40 +0200

    [deploy]

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,29 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import topsydeUtilsPlugin from "./plugins/topsyde-utils-vite-plugin";
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue(), tailwindcss(), topsydeUtilsPlugin()],
-	server: {
-		port: 8080,
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: "modern-compiler",
+export default defineConfig(({ command }) => {
+	return {
+		plugins: [vue(), tailwindcss(), topsydeUtilsPlugin()],
+		base: command === "serve" ? "/" : "/client_rune_dictionary/",
+		server: {
+			port: 8080,
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: "modern-compiler",
+				},
 			},
 		},
-	},
-	build: {
-		sourcemap: false,
-		minify: true,
-		assetsDir: "chunks",
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+		build: {
+			sourcemap: false,
+			minify: true,
+			assetsDir: "chunks",
 		},
-	},
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
+		},
+	};
 });
