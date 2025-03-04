26bc98836ce32d7c2daa69d2df5d67146bb7bb74
Liad Goren <liad@tigloo.io>
2025-03-04 21:52:03 +0200

    [deploy]

--- vite.config.ts
+++ vite.config.ts
@@ -3,57 +3,22 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import topsydeUtilsPlugin from "./plugins/topsyde-utils-vite-plugin";
-
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-	
-	return {
-		plugins: [vue(), tailwindcss(), topsydeUtilsPlugin()],
-		// Set base path conditionally - '/' for development, '/client_rune_dictionary/' for production
-		base: command === 'serve' ? '/' : '/client_rune_dictionary/',
-		server: {
-			port: 8080,
-			...(command === "serve" && {
-				fs: {
-					allow: [".."],
-				},
-			}),
-		},
-		css: {
-			preprocessorOptions: {
-				scss: {
-					api: "modern-compiler",
-				},
+export default defineConfig({
+	plugins: [vue(), tailwindcss(), topsydeUtilsPlugin()],
+	server: {
+		port: 8080,
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: "modern-compiler",
 			},
 		},
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "./src"),
-			},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
 		},
-		build: {
-			rollupOptions: {
-				// Don't externalize in development or production
-				external: [],
-				output: {
-					manualChunks: {
-						vendor: ["vue", "vue-router", "pinia", "pinia-plugin-persistedstate", "@vueuse/core"],
-						primevue: ["primevue", "@primevue/themes"],
-					},
-					chunkFileNames: "assets/[name]-[hash].js",
-					entryFileNames: "assets/[name]-[hash].js",
-					assetFileNames: "assets/[name]-[hash].[ext]",
-				},
-			},
-			minify: "terser",
-			terserOptions: {
-				compress: {
-					drop_console: false,
-					drop_debugger: true,
-				},
-			},
-			chunkSizeWarningLimit: 600,
-			sourcemap: false,
-		},
-	};
+	},
 });
