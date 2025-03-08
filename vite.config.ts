8d33424c5bb63d3c76aa15743b6ea0f17028b193
Liad Goren <liad@tigloo.io>
2025-03-08 14:27:39 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -26,19 +26,8 @@ export default defineConfig(({ command, mode }) => {
 					ws: true,
 					changeOrigin: true,
 					rewrite: (path) => path.replace(/^\/ws/, ""),
-					secure: false,
+					secure: true,
 					timeout: 60000,
-					configure: (proxy, _options) => {
-						proxy.on('error', (err, _req, _res) => {
-							console.log('proxy error', err);
-						});
-						proxy.on('proxyReq', (proxyReq, req, _res) => {
-							console.log('Sending Request to the Target:',proxyReq.path, req.method, req.url);
-						});
-						proxy.on('proxyRes', (proxyRes, req, _res) => {
-							console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-						});
-					},
 				},
 			},
 		},
