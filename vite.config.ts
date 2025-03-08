6691c4178a479660291339efbd871c21b897078c
Liad Goren <liad@tigloo.io>
2025-03-08 12:30:22 +0200

    Add cache control meta tags and update proxy logging

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig(({ command, mode }) => {
 							console.log('proxy error', err);
 						});
 						proxy.on('proxyReq', (proxyReq, req, _res) => {
-							console.log('Sending Request to the Target:', req.method, req.url);
+							console.log('Sending Request to the Target:',proxyReq.path, req.method, req.url);
 						});
 						proxy.on('proxyRes', (proxyRes, req, _res) => {
 							console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
