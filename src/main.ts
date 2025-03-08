cea6af188945cf626423834069a95a5c4eda977b
Liad Goren <liad@tigloo.io>
2025-03-08 12:46:46 +0200

    Version bump to 0.2.13 [deploy]

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import Ripple from "primevue/ripple";
 import ToastService from "primevue/toastservice";
 import DebugDirective from "./directives/debug.directive.ts";
 
-// @version: 0.2.12 - This comment is auto-updated by deploy script
+// @version: 0.2.13 - This comment is auto-updated by deploy script
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
