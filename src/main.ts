3f3f0be004e6d2873ed069f51193fa4cd73a3e40
Liad Goren <liad@tigloo.io>
2025-03-09 09:06:48 +0200

    Version bump to 0.2.22 [deploy]

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import Ripple from "primevue/ripple";
 import ToastService from "primevue/toastservice";
 import DebugDirective from "./directives/debug.directive.ts";
 
-// @version: 0.2.19 - This comment is auto-updated by deploy script
+// @version: 0.2.22 - This comment is auto-updated by deploy script
 
 // Force refresh when app version changes
 (function() {
