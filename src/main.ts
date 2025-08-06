3c0dd2414882a6352cdccf74b62e1943782f22fb
Liad Goren <khgs2411@gmail.com>
2025-08-06 22:24:35 +0300

    Version bump to 0.3.3 [deploy]

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import Ripple from "primevue/ripple";
 import ToastService from "primevue/toastservice";
 import DebugDirective from "./directives/debug.directive.ts";
 
-// @version: 0.3.2 - This comment is auto-updated by deploy script
+// @version: 0.3.3 - This comment is auto-updated by deploy script
 
 // Force refresh when app version changes
 (function () {
