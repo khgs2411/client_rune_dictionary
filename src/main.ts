788dd0a00a630fe349f2d8775c4f204a9f929aed
Liad Goren <khgs2411@gmail.com>
2025-10-14 21:56:55 +0300

    assets and chunks folder [deploy]

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router';
 import './style.css';
-// @version: 0.6.5 - This comment is auto-updated by deploy script
+// @version: 0.6.6 - This comment is auto-updated by deploy script
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
